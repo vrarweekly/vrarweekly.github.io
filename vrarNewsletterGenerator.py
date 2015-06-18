0434ee05f0f2bad29d38c224229697d8543c293d
tonysheng <tony.y.sheng@gmail.com>
2015-06-18 09:21:41 -0700

    add new week

--- vrarNewsletterGenerator.py
+++ vrarNewsletterGenerator.py
@@ -15,20 +15,21 @@ secondary_color = "#303F9F" # links
 support_color = "#607D8B"# domains 
 ## categories
 categories = [
-["Oculus", ["Oculus"]],
+["StarVR", ["StarVR"]],
+["Oculus", ["Oculus", "Palmer", "Rift"]],
 ["Google", ["Google", "YouTube", "Cardboard", "Android", "Android"]],
-["Valve", ["Valve", "Steam", "Vive", "HTC"]]
+["Valve", ["Valve", "Steam", "Vive", "HTC", "Lighthouse"]],
+["Microsoft", ["Microsoft", "Xbox", "Minecraft", "Hololens"]]
 ] # Leave empty if you don't want categories. If you have categories, add them to the list in the following format: [name of category, [flair tag 1, flair tag 2, flair tag3, ...]]. It will search each flair tag and if there's a match, assign it to the category.
 category_search_method = "title" # choose title or flair depending
 
 def newIssue(issue,date):
 	newsletter(issue,date)
 	# update the html of the landing page
-	# create a markdown version to post on reddit
 
 def newsletter(issue, date):
 	
-	content = generatePosts('json/'+date+'.json')
+	html_content, markdown_content = generatePosts('json/'+date+'.json')
 	email_html = """
 <!DOCTYPE html>
 <html>
@@ -58,7 +59,7 @@ def newsletter(issue, date):
 		<!-- POSTS CONTAINER -->
 		<tr><td style="padding: 16px 12px 12px 12px" align="left">
 
-		{{content}}
+		{{html_content}}
 
 		</td></tr>
 
@@ -76,40 +77,63 @@ def newsletter(issue, date):
 """
 	email_html = email_html.replace("{{issue}}", issue)
 	email_html = email_html.replace("{{date}}", date)
-	email_html = email_html.replace("{{content}}", content)
+	email_html = email_html.replace("{{html_content}}", html_content)
 	email_html = email_html.replace("{{newsletter_name}}", newsletter_name)
 	email_html = email_html.replace("{{primary_color}}", primary_color)
 	email_html = email_html.replace("{{secondary_color}}", secondary_color)
 	email_html = email_html.replace("{{support_color}}", support_color)
+
+	#markdown 
+	markdown_text = """
+#{{newsletter_name}}
+Issue #{{issue}} // {{date}}
+[Subscribe to get every issue in your inbox](http://www.vrarweekly.com)
+
+{{markdown_content}}
+
+This is an experiment that will improve every week. Reply to this thread to share feedback. 
+"""
+	markdown_text = markdown_text.replace("{{issue}}", issue)
+	markdown_text = markdown_text.replace("{{date}}", date)
+	markdown_text = markdown_text.replace("{{markdown_content}}", markdown_content)
+	markdown_text = markdown_text.replace("{{newsletter_name}}", newsletter_name)
 
 	Html_file = open(issue+".html","w")
 	print "created newsletter '"+issue+"'"
 	Html_file.write(email_html.encode('utf-8'))
 	Html_file.close()
 
+	Html_file = open("md/"+issue+".md","w")
+	print "created markdown '"+issue+"'"
+	Html_file.write(markdown_text.encode('utf-8'))
+	Html_file.close()
+
 def generatePosts(file):
 	posts_html = """
 	"""
-	
+	posts_markdown = """
+	"""
+
 	json_data = open(file)
 	file_data = json.load(json_data)
 	post_list = [];
 
 	for post in file_data['data']['children']:
-		post_title = post['data']['title']
-		post_url = post['data']['url']
-		post_description = post['data']['selftext']
+		post_data = {}
+		post_title = post['data']['title'].encode('utf-8')
+		post_url = post['data']['url'].encode('utf-8')
+		post_description = post['data']['selftext'].encode('utf-8')
 		if len(post_description) > 300:
 			post_description = post_description[0:300] + "[...]"
-		post_domain = post['data']['domain']
-		post_permalink = "https://www.reddit.com" + post['data']['permalink']
+		post_domain = post['data']['domain'].encode('utf-8')
+		post_permalink = "https://www.reddit.com" + post['data']['permalink'].encode('utf-8')
 		post_flair = str(post['data']['link_flair_text'])
 		post_comments = str(post['data']['num_comments'])
 		post_html = """
 		<table width="600" align="left" class="gowide" cellpadding="0" cellspacing="0" style="border-collapse: collapse"><tr><td>
-				<div style="font-size: 18px; margin: 2px 0px"><a target="_blank" href="{{post_url}}?utm_source=ezkappa&amp;utm_medium=email" title="{{post_url}}" style="display: block; color: #0088cc; line-height: 24px; text-decoration: underline; font-weight: 500">{{post_title}}</a></div>
+				<div style="font-size: 18px; margin: 2px 0px"><a target="_blank" href="{{post_url}}?utm_source=vrarweekly&amp;utm_medium=email" title="{{post_url}}" style="display: block; color: #0088cc; line-height: 24px; text-decoration: underline; font-weight: 500">{{post_title}}</a></div>
 				<div style="color: {{support_color}}; font-weight: normal; font-size: 12px; line-height: 24px; text-transform: lowercase; margin-top: 2px; font-family: verdana, arial, sans-serif">
-					{{post_domain}}   <a target="_blank" href="{{post_permalink}}?utm_source=ezkappa&amp;utm_medium=email" title="{{post_permalink}}" style="display: inline-block; color: #0088cc; text-decoration: none; text-transform: none">  comments ({{post_comments}}) >></a></div>
+					{{post_domain}}   <a target="_blank" href="{{post_permalink}}?utm_source=vrarweekly&amp;utm_medium=email" title="{{post_permalink}}" style="display: inline-block; color: #0088cc; text-decoration: none; text-transform: none">  comments ({{post_comments}}) >></a></div>
 					<div style="font-size: 13px; line-height: 17px; margin-top: 4px; color: #666666">
 						{{post_description}}
 					</div>
@@ -118,52 +142,83 @@ def generatePosts(file):
 				<table width="140" class="nomo" align="left"><tr><td style="text-align: center"></td></tr></table>
 				<br clear="all" style="clear: both">
 		"""
-		post_html = post_html.replace("{{post_url}}", post_url)
-		post_html = post_html.replace("{{post_title}}", post_title)
-		post_html = post_html.replace("{{post_domain}}", post_domain)
+		post_markdown = """
+**[{{post_title}}]({{post_url}})**
+{{post_domain}} | [comments ({{post_comments}})]({{post_permalink}})
+{{post_description}}
+"""
+
+		post_html = post_html.replace("{{post_url}}", post_url.decode("utf-8"))
+		post_html = post_html.replace("{{post_title}}", post_title.decode("utf-8"))
+		post_html = post_html.replace("{{post_domain}}", post_domain.decode("utf-8"))
 		post_html = post_html.replace("{{post_flair}}", post_flair)
 		post_html = post_html.replace("{{post_comments}}", post_comments)
-		post_html = post_html.replace("{{post_description}}", post_description)
-		post_html = post_html.replace("{{post_permalink}}", post_permalink)
+		post_html = post_html.replace("{{post_description}}", post_description.decode("utf-8"))
+		post_html = post_html.replace("{{post_permalink}}", post_permalink.decode("utf-8"))
 
-		if category_search_method == "flair":
-			post_list.append([post_flair, post_html])
-		else:
-			post_list.append([post_title, post_html])
+		post_markdown = post_markdown.replace("{{post_url}}", post_url.decode("utf-8"))
+		post_markdown = post_markdown.replace("{{post_title}}", post_title.decode("utf-8"))
+		post_markdown = post_markdown.replace("{{post_domain}}", post_domain.decode("utf-8"))
+		post_markdown = post_markdown.replace("{{post_flair}}", post_flair)
+		post_markdown = post_markdown.replace("{{post_comments}}", post_comments)
+		post_markdown = post_markdown.replace("{{post_description}}", post_description.decode("utf-8"))
+		post_markdown = post_markdown.replace("{{post_permalink}}", post_permalink.decode("utf-8"))
+
+		post_list.append([post_title, post_html, post_flair, post_markdown])
 		
 	# if there are no categories
 	if len(categories) == 0:
 		for posts in post_list:
 			posts_html = posts_html + posts[1]
+			posts_markdown = posts_markdown + posts[3]
 	# if you need fuzzy search
-	elif categories[0][1]:		
+	elif categories[0][1]:
 		# add posts from each category
 		for category in categories:
 			category_post_list = []
 			for post in post_list:
 				for category_string in category[1]:
 					if (post[0] in category_string or category_string in post[0]):
-						post[0] = category[0]
-						category_post_list.append([post[0], post[1]])
+						category_post_list.append([post[0], post[1], post[2], post[3]])
+						post[0] = category[0] # effectively removes post from list
 			if len(category_post_list) != 0:
 				posts_html = posts_html + '<p style="color:{{primary_color}}; font-size: 24px; font-weight: normal; margin-top: 8px; margin-bottom: 10px">{{category_title}}</p>'
 				posts_html = posts_html.replace("{{category_title}}", category[0])
+
+				posts_markdown = posts_markdown + """
+##{{category_title}}
+"""
+				posts_markdown = posts_markdown.replace("{{category_title}}", category[0])
 				for post in category_post_list:
 					posts_html = posts_html + post[1]
+					posts_markdown = posts_markdown + post[3]
 		# add uncategorized posts
 		for post in post_list:
 			category_post_list = []
-			if post[0] not in categories:
-				category_post_list = category_post_list + [post[1]]
-		if len(category_post_list) != 0:
+			for category in categories:
+				if post[0] == category[0]:
+					post_list.remove(post)
+		for post in post_list:
+			for category in categories:
+				if post[0] == category[0]:
+					post_list.remove(post)
+		for post in post_list:
+			category_post_list.append(post)
+		if len(category_post_list) > 0:
 			posts_html = posts_html + '<p style="color:{{primary_color}}; font-size: 24px; font-weight: normal; margin-top: 8px; margin-bottom: 10px">Other</p>'
+			posts_markdown = posts_markdown + """
+##Other
+"""
 		for post in category_post_list:
-			posts_html = posts_html + post
-	# else serach for category in the title/flair
+			posts_html = posts_html + post[1]
+			posts_markdown = posts_markdown + post[3]
+	# else search for category using flair
 
-	return posts_html
 
-newIssue('1', 'June 11, 2015')
+
+	return (posts_html, posts_markdown)
+
+newIssue('2', 'June 18, 2015')
 
 
 
